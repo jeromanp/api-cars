8acd97f9a771f4178c182b333825763b2f860c35
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-02-20 23:29:50 -0600

    title, subtitke

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+"use client";
+
+import React from "react";
+import { CustomButton } from "@/components";
 
 const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+  const handleScroll = ()=>{
 
-export default Hero
+  }
+
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+          Busca, encuentra y renta cualquier vehículo
+        </h1>
+        <p className="hero__subtitle">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+          minus dolores voluptas necessitatibus ullam, ratione.
+        </p>
+
+        
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
