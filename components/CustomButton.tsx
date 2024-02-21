ecf4759c999d6b0c87b377f9ac5ddb9ab756ce49
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-02-21 12:26:51 -0600

    apply handleClick and btnType

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -2,13 +2,13 @@
 import { CustomButtonProps } from "@/types";
 import React from "react";
 
-const CustomButton = ({ title, containerStyles }: CustomButtonProps) => {
+const CustomButton = ({ title, containerStyles, handleClick, btnType }: CustomButtonProps) => {
   return (
     <button
       disabled={false}
-      type={"button"}
+      type={btnType || "button"}
       className={`custom-btn ${containerStyles}`}
-      onClick={() => {}}
+      onClick={handleClick}
     >
       <span className="flex-1">{title}</span>
     </button>
