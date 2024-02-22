5873f3c91cdc95f432d787b0ee4144a29c8ce2e8
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-02-21 23:01:12 -0600

    add footer links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -19,11 +19,30 @@ const Footer = () => {
             RoPi Car 2024 <br /> Todos los derechos reservados &copy;{" "}
           </p>
         </div>
-        
+        <div className="footer__links">
+          {footerLinks.map((link) => (
+            <div key={link.title} className="footer__link">
+              <h3 className="font-bold">{link.title}</h3>
+              {link.links.map((item) => (
+                <Link
+                  key={item.title}
+                  href={item.url}
+                  className="text-gray-500"
+                >
+                  {item.title}
+                </Link>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10">
+        <p>@2023 RoPi Car. Todos los derechos reservados</p>
 
         <div className="footer__copyrights-link">
           <Link href="/" className="text-gray-500">
-            Política de privacidad
+            Política de Privacidad
           </Link>
           <Link href="/" className="text-gray-500">
             Terminos y Condiciones
