be2c2ec0bfb289c87a5e87d58122b08177ce9a16
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-02-22 00:21:02 -0600

    design page home

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,18 @@ export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero />
+
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home__text-container">
+          <h1 className="text-4xl font-extrabold">Catálogo de Coches</h1>
+          <p>Explora los coches de acuerdo a tus gustos</p>
+        </div>
+
+        <div className="home__filters">
+          {/* <SeachBar/> */}
+          <div className="home__filter-container">{/* <CustomFilter/> */}</div>
+        </div>
+      </div>
     </main>
   );
 }
