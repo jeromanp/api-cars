7074a2b4439c4345c3e0b0e341d91939c9cc554a
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-02-21 22:42:53 -0600

    design footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="flex flex-col text-black-100 mt-5 border-t border-gray-100">
+      <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
+        <div className="flex flex-col justify-start items-start gap-6">
+          <Image
+            src="/logo.svg"
+            alt="Footer logo"
+            width={118}
+            height={18}
+            className="object-contain"
+          />
+          <p className="text-base text-gray-700">
+            RoPi Car 2024 <br /> Todos los derechos reservados &copy;{" "}
+          </p>
+        </div>
+        <div className="footer__links"></div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
