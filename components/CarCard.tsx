2590b2d1bf20d0d04a2066eadfb2cb4b703b07ab
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-02-24 00:13:38 -0600

    import type carProps

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
+"use client";
 
-const CarCard = () => {
-  return (
-    <div>
-      CarCard
-    </div>
-  )
+import React, { useState } from "react";
+import Image from "next/image";
+import { CarProps } from "@/types";
+
+interface CarCardProps {
+  car: CarProps;
 }
 
-export default CarCard
+const CarCard = ({ car }: CarCardProps) => {
+  return <div>CarCard</div>;
+};
+
+export default CarCard;
