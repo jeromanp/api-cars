522956ae766cd520ff59a7e881c7178bb0f555e5
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-02-27 00:31:52 -0600

    styles for modal

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -39,8 +39,12 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <Dialog.Panel>
-                  <button type="button" onClick={closeModal}>
+                <Dialog.Panel className="relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white text-left shadow-xs transition-all flex flex-col gap-5">
+                  <button
+                    type="button"
+                    onClick={closeModal}
+                    className="absolute top-2 right-2 z-10 w-fit bg-primary-blue-100 rounded-full"
+                  >
                     <Image
                       src="/close.svg"
                       alt="close"
