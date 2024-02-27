34754c9c7ccf83ce3b6e63aec6ca9b83757ee574
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-02-27 00:46:02 -0600

    style for carDetail

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -63,6 +63,35 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                         className="object-contain"
                       />
                     </div>
+                    <div className="flex gap-3">
+                      <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-100">
+                        <Image
+                          src="/hero.png"
+                          alt="Detalle coche"
+                          fill
+                          priority
+                          className="object-contain"
+                        />
+                      </div>
+                      <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-100">
+                        <Image
+                          src="/hero.png"
+                          alt="Detalle coche"
+                          fill
+                          priority
+                          className="object-contain"
+                        />
+                      </div>
+                      <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-100">
+                        <Image
+                          src="/hero.png"
+                          alt="Detalle coche"
+                          fill
+                          priority
+                          className="object-contain"
+                        />
+                      </div>
+                    </div>
                   </div>
                 </Dialog.Panel>
               </Transition.Child>
