da006de0c79fa8bf0daf290f3a19a2680799364b
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-03-12 11:00:34 -0600

    fix listbox

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -7,15 +7,20 @@ import { Listbox, Transition } from '@headlessui/react'
 import { CustomFilterProps } from '@/types'
 
 const CustomFilter = ({title, options}:CustomFilterProps) => {
+  const [selected, setSelected] = useState(options[0])
+
   return (
     <div className='w-fit'>
-      <div className='relative w}fit z-10'>
-        <Listbox.Button className="custom-bilter_btn">
+      <Listbox>
+
+      <div className='relative w-fit z-10'>
+        <Listbox.Button className="custom-bilter__btn">
           <span>Filter</span>
         </Listbox.Button>
         
       </div>
 
+      </Listbox>
     </div>
   )
 }
