9ed800b9dbb3788bc54ac453007e8bfa19b8bda5
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-03-13 10:47:41 -0600

    add delete params

--- utils/index.ts
+++ utils/index.ts
@@ -1,26 +1,23 @@
 import { CarProps, FilterProps } from "@/types";
 
-export async function fetchCars(filters:FilterProps) {
-  const {manufacturer, year, model, limit, fuel}= filters
-  try {
-    let headers: HeadersInit = {
-      "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
-    };
+export async function fetchCars(filters: FilterProps) {
+  const { manufacturer, year, model, limit, fuel } = filters;
 
-    if (process.env.NEXT_PUBLIC_API_RAPID_KEY) {
-      headers["X-RapidAPI-Key"] = process.env.NEXT_PUBLIC_API_RAPID_KEY;
+  const headers: HeadersInit = {
+    "X-RapidAPI-Key": process.env.NEXT_PUBLIC_RAPID_API_KEY || "",
+    "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+  };
+
+  const response = await fetch(
+    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
+    {
+      headers: headers,
     }
+  );
 
-    const response = await fetch(
-      `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
-      { headers: headers }
-    );
+  const result = await response.json();
 
-    const result = await response.json();
-    return result;
-  } catch (error) {
-    console.error(error);
-  }
+  return result;
 }
 
 export const calculateCarRent = (city_mpg: number, year: number) => {
@@ -42,24 +39,38 @@ export const generateCarImageUrl = (car: CarProps, angle?: string) => {
   const url = new URL("https://cdn.imagin.studio/getimage");
   const { make, model, year } = car;
 
-  url.searchParams.append('customer', process.env.NEXT_PUBLIC_IMAGIN_API_KEY || '');
-  url.searchParams.append('make', make);
-  url.searchParams.append('modelFamily', model.split(" ")[0]);
-  url.searchParams.append('zoomType', 'fullscreen');
-  url.searchParams.append('modelYear', `${year}`);
+  url.searchParams.append(
+    "customer",
+    process.env.NEXT_PUBLIC_IMAGIN_API_KEY || ""
+  );
+  url.searchParams.append("make", make);
+  url.searchParams.append("modelFamily", model.split(" ")[0]);
+  url.searchParams.append("zoomType", "fullscreen");
+  url.searchParams.append("modelYear", `${year}`);
   // url.searchParams.append('zoomLevel', zoomLevel);
-  url.searchParams.append('angle', `${angle}`);
+  url.searchParams.append("angle", `${angle}`);
 
   return `${url}`;
-} 
+};
 
-
-export const updateSearchParams = (type:string, value:string)=>{
+export const updateSearchParams = (type: string, value: string) => {
   const searchParams = new URLSearchParams(window.location.search);
-  
-  searchParams.set(type, value)
 
-  const newPathname = `${window.location.pathname}?${searchParams.toString()}`
+  searchParams.set(type, value);
+
+  const newPathname = `${window.location.pathname}?${searchParams.toString()}`;
 
   return newPathname;
-}
+};
+
+export const deleteSearchParams = (type: string) => {
+  const newSearchParams = new URLSearchParams(window.location.search);
+
+  newSearchParams.delete(type.toLocaleLowerCase());
+
+  const newPathname = `${
+    window.location.pathname
+  }?${newSearchParams.toString()}`;
+
+  return newPathname;
+};
