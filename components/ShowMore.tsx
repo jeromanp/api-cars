811ed47fb485b38dc8b69bfa1ef642a481a5602c
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-03-15 12:02:44 -0600

    show more ok

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -1,11 +1,33 @@
-import React from 'react'
+"use client";
 
-const ShowMore = () => {
+import React from "react";
+import { useRouter } from "next/navigation";
+import { ShowMoreProps } from "@/types";
+import { CustomButton } from "@/components";
+import { updateSearchParams } from "@/utils";
+
+const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
+  const router = useRouter();
+
+  const handleNavigation = () => {
+    const newLimit = (pageNumber + 1) * 10;
+    const newPathname = updateSearchParams("limit", `${newLimit}`);
+
+    router.push(newPathname);
+  };
+
   return (
-    <div>
-      
+    <div className="w-full flex-center gap-5 mt-10">
+      {!isNext && (
+        <CustomButton
+          btnType="button"
+          title="Show More"
+          containerStyles="bg-primary-blue rounded-full text-white"
+          handleClick={handleNavigation}
+        />
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default ShowMore
+export default ShowMore;
