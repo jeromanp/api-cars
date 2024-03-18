301901aa2a35e89ffdb430d113f01148e7e1fbef
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-03-17 23:51:43 -0600

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
+import Head from "next/head";
 import { Navbar, Footer } from "@/components";
 
 export const metadata: Metadata = {
@@ -15,6 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>       
+        <link rel="icon" href="/RoPiCars.PNG" sizes="any" />
+      </Head>
       <body className="relative">
         <Navbar/>
         {children}
