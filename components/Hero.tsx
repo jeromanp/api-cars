a61191ff0f71cbb3f0f132aeb6ccc59be6642921
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-03-17 23:27:26 -0600

    delete /impoprt handleScroll

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,24 +3,21 @@
 import Image from "next/image";
 import React from "react";
 import { CustomButton } from "@/components";
+import { handleScroll } from "@/utils";
 
 const Hero = () => {
-  const handleScroll = () => {
-    const nextSection = document.getElementById("discover");
-
-    if (nextSection) {
-      nextSection.scrollIntoView({ behavior: "smooth" });
-    }
-  };
-
   return (
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
         <h1 className="hero__title text-center  ">
-          <b className="text-primary-blue">Buscador</b><br/> de<br/> <b className="text-primary-blue">vehículos</b>.
+          <b className="text-primary-blue">Buscador</b>
+          <br /> de
+          <br /> <b className="text-primary-blue">vehículos</b>.
         </h1>
         <p className="hero__subtitle text-justify">
-          Este proyecto se encamina a visualizar resultador extraidos de API-Ninjas en su versión de coches. Podemos filtrar vehículos por marca, modelo o tipo de combustible.
+          Este proyecto se encamina a visualizar resultador extraidos de
+          API-Ninjas en su versión de coches. Podemos filtrar vehículos por
+          marca, modelo o tipo de combustible.
         </p>
 
         <CustomButton
