8e745a3a0c964b87d437413421f1a847c9cb7e6c
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-03-17 23:11:53 -0600

    format text hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -16,12 +16,11 @@ const Hero = () => {
   return (
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
-        <h1 className="hero__title">
-          Busca, encuentra y renta cualquier vehículo
+        <h1 className="hero__title text-center  ">
+          <b className="text-primary-blue">Buscador</b><br/> de<br/> <b className="text-primary-blue">vehículos</b>.
         </h1>
-        <p className="hero__subtitle">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
-          minus dolores voluptas necessitatibus ullam, ratione.
+        <p className="hero__subtitle text-justify">
+          Este proyecto se encamina a visualizar resultador extraidos de API-Ninjas en su versión de coches. Podemos filtrar vehículos por marca, modelo o tipo de combustible.
         </p>
 
         <CustomButton
