57178ebd5719b1810a5177212e030bb57c055dd1
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-03-18 22:28:37 -0600

    fix title

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -34,15 +34,15 @@ year:1993
   return (
     <div className="car-card group">
       <div className="car-card__content">
-        <h2 className="car-card__content-titke">
+        <h2 className="car-card__content-title">
           {make} {model}
         </h2>
       </div>
 
       <p className="flex mt-6 text-[32px] font-extrabold">
-        <span className="self-start text-[14px] font-semibold">$</span>
+        {/* <span className="self-start text-[14px] font-semibold">$</span>
         {carRent}
-        <span className="self-end text-[14px] font-medium">/day</span>
+        <span className="self-end text-[14px] font-medium">/day</span> */}
       </p>
 
       <div className="relative w-full h-40 my-3 object-contain">
